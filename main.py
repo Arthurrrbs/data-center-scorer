9f6fbdec591e6b5121e0df4ee7ebd85d513c9265
Arthurrrbs <arthur.ribes@sciencespo.fr>
2025-05-01 22:17:19 +0200

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ code_insee = "34172"
 annee = "2022"
 data = []
 
-with st.spinner("🔍 Récupération des données Enedis pour Montpellier (tous secteurs)..."):
+with st.spinner("🔍 Récupération des données Enedis pour Mocccntpellier (tous secteurs)..."):
     url = "https://data.enedis.fr/api/records/1.0/search/"
     params = {
         "dataset": "consommation-electrique-par-secteur-dactivite-commune",
