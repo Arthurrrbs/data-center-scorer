e050999768b20d1be241deffd55d9c90234f655e
Arthurrrbs <arthur.ribes@sciencespo.fr>
2025-05-02 15:40:14 +0200

    Update enedis_downloader.py

--- scripts/enedis_downloader.py
+++ scripts/enedis_downloader.py
@@ -10,7 +10,7 @@ today = datetime.now().strftime("%Y-%m-%d")
 filename = f"data/enedis_conso_communes_{today}.csv"
 url = "https://data.enedis.fr/explore/dataset/consommation-electrique-par-secteur-dactivite-commune/download/?format=csv&timezone=Europe/Berlin&use_labels_for_header=true"
 
-print("📥 Démarrage du téléchargement Enedis en mode streaming...")
+print("📥 Démarrage du téléchargement Enedis en mode streaming...", flush=True)
 
 try:
     with requests.get(url, stream=True, timeout=60) as r:
@@ -19,6 +19,7 @@ try:
             for chunk in r.iter_content(chunk_size=8192):
                 if chunk:
                     f.write(chunk)
+                            print(".", end="", flush=True)
     print(f"✅ Données enregistrées dans {filename}")
 except Exception as e:
     print(f"❌ Erreur pendant le téléchargement : {e}")
